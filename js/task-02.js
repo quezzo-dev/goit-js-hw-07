d27f1cf7d63ae588d4a270868041a7bf68c74180
Vladyslav Prokopenko <main.prokopenko@gmail.com>
2021-09-01 23:30:12 +0300

    Task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Картошка',
-  'Грибы',
-  'Чеснок',
-  'Помидоры',
-  'Зелень',
-  'Приправы',
+  "Картошка",
+  "Грибы",
+  "Чеснок",
+  "Помидоры",
+  "Зелень",
+  "Приправы",
 ];
+
+const listEl = document.querySelector("#ingredients");
+
+const addEl = (array) =>
+  array.map((el) => {
+    const itemEl = document.createElement("li");
+    itemEl.textContent = el;
+    return itemEl;
+  });
+
+listEl.append(...addEl(ingredients));
